1a5ec12700ab615d29a482f18bf2cc0367d284a7
ryanlennon <ryanlennon07@gmail.com>
2016-02-03 23:47:07 -0600

    put accordion javascript in its own file

---
+++ javascript/accordion/accordion.js
@@ -0,0 +1,9 @@
+$(document).ready(function($) {
+    $('.accordion-wrapper').find('.accordion-header').click(function() {
+
+      $(this).next().slideToggle();
+
+      $(".accordion-content").not($(this).next()).slideUp();
+
+    });
+});
