ad3147bf98ed062d9583b0b0c9f90e6465f66b49
ryanlennon <ryanlennon07@gmail.com>
2016-02-03 23:31:00 -0600

    Completed blackjack tests using minitest and rspec, completed the accordion feature, and defined the where method

--- ruby/where/test.rb
+++ ruby/where/test.rb
@@ -1,17 +1,27 @@
 require 'minitest/autorun'
 
+class Array 
+  
+  def where(input)
+    self.select {|fixture| fixture[input] === input }
+  end
+end
+
 class WhereTest < Minitest::Test
+
   def setup
-    @boris   = {:name => 'Boris The Blade', :quote => "Heavy is good. Heavy is reliable. If it doesn't work you can always hit them.", :title => 'Snatch', :rank => 4}
+    @boris = {:name => 'Boris The Blade', :quote => "Heavy is good. Heavy is reliable. If it doesn't work you can always hit them.", :title => 'Snatch', :rank => 4}
     @charles = {:name => 'Charles De Mar', :quote => 'Go that way, really fast. If something gets in your way, turn.', :title => 'Better Off Dead', :rank => 3}
-    @wolf    = {:name => 'The Wolf', :quote => 'I think fast, I talk fast and I need you guys to act fast if you wanna get out of this', :title => 'Pulp Fiction', :rank => 4}
-    @glen    = {:name => 'Glengarry Glen Ross', :quote => "Put. That coffee. Down. Coffee is for closers only.",  :title => "Blake", :rank => 5}
+    @wolf = {:name => 'The Wolf', :quote => 'I think fast, I talk fast and I need you guys to act fast if you wanna get out of this', :title => 'Pulp Fiction', :rank => 4}
+    @glen = {:name => 'Glengarry Glen Ross', :quote => "Put. That coffee. Down. Coffee is for closers only.",  :title => "Blake", :rank => 5}
 
     @fixtures = [@boris, @charles, @wolf, @glen]
+
+
   end
 
   def test_where_with_exact_match
-    assert_equal [@wolf], @fixtures.where(:name => 'The Wolf'),
+    assert_equal [@wolf], @fixtures.where(:name => 'The Wolf')
   end
 
   def test_where_with_partial_match
