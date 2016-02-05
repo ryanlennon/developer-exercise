2a3ae391853328b6d0573ed57c4cce8262664300
ryanlennon <ryanlennon07@gmail.com>
2016-02-05 10:41:21 -0600

    updated model,controller,and view in backbone

--- javascript/accordion/tests.js
+++ javascript/accordion/tests.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
   module("Accordion tests");
 
   test("example test", function() {
--- ruby/youtube/youtube_controller.rb
+++ ruby/youtube/youtube_controller.rb
@@ -1,4 +1,5 @@
 class TubesController
+  require 'unirest'
 
   def index
 
